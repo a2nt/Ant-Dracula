098b30497be44186cec2c6aab218f850b8ecf6af
Tony Air <tony@thewebdevelopmentagency.com>
2018-03-10 02:51:37 +0700

    Minor updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,24 +3,29 @@ var sass = require('gulp-sass');
 var exec = require('gulp-exec');
 
 gulp.task('styles', function() {
+    gulp.src('gtk-3.0/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./gtk-3.0/'))
+
     gulp.src('gtk-3.20/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gtk-3.20/'))
-        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Dracula"'))
+        .pipe(exec('gsettings set org.gnome.desktop.interface gtk-theme "Default" && gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula-Purple"'))
 });
 gulp.task('shell-style', function() {
     gulp.src('gnome-shell/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gnome-shell/'))
-        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Ant"'))
-        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Dracula"'))
+        .pipe(exec('gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula-Purple"'))
+        .pipe(exec('gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula-Purple"'))
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('gtk-3.20/**/*.scss',['styles']);
+gulp.task('default', function() {
+    gulp.watch('gtk-3.0/**/*.scss', ['styles']);
+    gulp.watch('gtk-3.20/**/*.scss', ['styles']);
 });
 
-gulp.task('shell',function() {
-    gulp.watch('gnome-shell/*.scss',['shell-style']);
+gulp.task('shell', function() {
+    gulp.watch('gnome-shell/*.scss', ['shell-style']);
 });
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Extract the zip file to the themes directory i.e. `/usr/share/themes/` or `~/.th
 To set the theme in Gnome, run the following commands in Terminal,
 
 ```
-gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula"
-gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula"
+gsettings set org.gnome.desktop.interface gtk-theme "Ant-Dracula-Purple"
+gsettings set org.gnome.desktop.wm.preferences theme "Ant-Dracula-Purple"
 ```
 or Change via distribution specific tool.
 
